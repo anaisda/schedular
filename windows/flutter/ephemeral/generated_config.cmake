12dd5683492131737d7e865a3d60f1331dfc2ee4
anaisda <125089416+anaisda@users.noreply.github.com>
2024-04-04 18:36:16 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter_windows_3.13.7-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\AndroidStudioProjects\\GP\\schedular" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter_windows_3.13.7-stable\\flutter"
+  "PROJECT_DIR=D:\\AndroidStudioProjects\\GP\\schedular"
+  "FLUTTER_ROOT=D:\\flutter_windows_3.13.7-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\AndroidStudioProjects\\GP\\schedular\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\AndroidStudioProjects\\GP\\schedular"
+  "FLUTTER_TARGET=D:\\AndroidStudioProjects\\GP\\schedular\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8zOWM2ZGM5YmRkM2JhMmJjMzg3ZTZlMTRiNzJmY2U1ZjQ2YWJmY2FlLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\AndroidStudioProjects\\GP\\schedular\\.dart_tool\\package_config.json"
+)
